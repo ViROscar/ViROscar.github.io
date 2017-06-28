06f517a2f18d58c59418f3d6a4bc266c5f5ee171
ViROscar <oscarvir123@gmail.com>
2017-06-28 16:00:28 -0500

    ww
    ww

--- todo.js
+++ todo.js
@@ -7,7 +7,7 @@ angular.module('todoApp', [])
 	$http({
 		url: "https://getbible.net/json?scripture=Psa%20119:4-16;23:1-6&v=valera",
 		method: "GET"
-	}).then(function s(data) {
+	}).then(function (data) {
 		$scope.Verso = data;
 	});
 });
