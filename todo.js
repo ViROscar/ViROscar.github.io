2cbbf9e7da9f73c6d3c9a2c5ed51fa32a3b00103
ViROscar <oscarvir123@gmail.com>
2017-06-28 14:36:07 -0500

    asd
    asd

--- todo.js
+++ todo.js
@@ -1,6 +1,5 @@
 angular.module('todoApp', [])
 .controller('TodoController', function ($scope, $http) {
-	var todoC = this.$scope;
-	todoC.Texto="";
-	todoC.Sufijo="Senior";
+	$scope.Texto="";
+	$scope.Sufijo="Senior";
 });
