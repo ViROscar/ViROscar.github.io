c75e96c863df61403d473f3e0470c8927924aacc
ViROscar <oscarvir123@gmail.com>
2017-06-28 16:26:03 -0500

    w
    ww

--- todo.js
+++ todo.js
@@ -4,10 +4,16 @@ angular.module('todoApp', [])
 	$scope.Sufijo = "Senior";
 
 	$scope.Verso = "";
+
 	$http({
-		url: "https://getbible.net/json?scripture=Psa%20119:4-16;23:1-6&v=valera",
-		method: "GET"
-	}).then(function (data) {
+		method: 'GET',
+		dataType: 'json',
+		url: 'https://getbible.net/json?scripture=Psa%20119:4-16;23:1-6&v=valera'
+	}).then(function successCallback(response) {
 		$scope.Verso = data;
+	}, function errorCallback(response) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
 	});
+
 });
