7f4333bed04998307faaa40b220201b18863448d
ViROscar <oscarvir123@gmail.com>
2017-06-28 15:27:05 -0500

    ww
    ww

--- todo.js
+++ todo.js
@@ -1,5 +1,18 @@
 angular.module('todoApp', [])
 .controller('TodoController', function ($scope, $http) {
-	$scope.Texto="";
-	$scope.Sufijo="Senior";
+	$scope.Texto = "";
+	$scope.Sufijo = "Senior";
+
+	$scope.Verso = "";
+	$http({
+			url: user.details_path,
+			method: "GET",
+			params: {
+				scripture: "Psa1",
+				v:"valera"
+			}
+		}).success(function(data) {
+        
+        $scope.Verso = data;
+    });;
 });
