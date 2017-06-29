2df817d9088dbe3a819d2a69133860e9005fce1e
ViROscar <oscarvir123@gmail.com>
2017-06-28 23:30:18 -0500

    www
    www

--- todo.js
+++ todo.js
@@ -1,19 +1,13 @@
 angular.module('todoApp', [])
-.controller('TodoController', function ($scope, $http) {
+.controller('TodoController', function ($scope, $http, $sce) {
 	$scope.Texto = "";
 	$scope.Sufijo = "Senior";
 
 	$scope.Verso = "";
-
-	$http({
-		method: 'GET',
-		dataType: 'json',
-		url: 'https://getbible.net/json?scripture=Psa%20119:4-16;23:1-6&v=valera'
-	}).then(function successCallback(response) {
-		$scope.Verso = data;
-	}, function errorCallback(response) {
-		// called asynchronously if an error occurs
-		// or server returns response with an error status.
-	});
+	
+	$http.jsonp($sce.trustAsResourceUrl("http://getbible.net/json?p=John1&v=valera"), {jsonpCallbackParam: 'callback'})
+    .then(function(data){
+        $scope.Verso = data.data;
+    });
 
 });
